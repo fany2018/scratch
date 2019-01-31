8a882ac25a5e513a86f019bc89fe19605b9324e7
fany2018 <42176664+fany2018@users.noreply.github.com>
2019-01-31 20:59:17 +0000

    Update download-excel.py

--- download-excel.py
+++ download-excel.py
@@ -1,9 +1,11 @@
 import ssl
 import pandas as pd
-import datetime
+from datetime import date, timedelta
 
 ssl._create_default_https_context = ssl._create_unverified_context
-df = pd.read_excel('https://www.nationalgridgas.com/sites/gas/files/documents/ST%20' + datetime.datetime.now().strftime("%d.%m.%Y") + '.xls', sheet_name='Today')
+df = pd.read_excel('https://www.nationalgridgas.com/sites/gas/files/documents/ST%20' + date.today().strftime("%d.%m.%Y") + '.xls', sheet_name='Today')
 
-print("Column headings:")
-print(df.columns)
+print(df)
+
+df2 = pd.read_csv('https://www.interconnector.com/Data/ISIS/flowdata/interconnector_historical_' + date.today().strftime("%Y%m") + '.txt')
+print(df2[df2['Gasday']==(date.today() - timedelta(1)).strftime("%d/%m/%Y")])
